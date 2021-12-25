7d61d6ab094c6003af0057d7366dddb775eec0a4
Sérgio Dias <up201704889@fe.up.pt>
2021-12-25 12:19:59 +0000

    feat(ui): added name and contacts to the resumeInfo page

--- client/src/pages/ResumeInfo.tsx
+++ client/src/pages/ResumeInfo.tsx
@@ -1,5 +1,31 @@
 import React, { useEffect, useState, } from 'react';
 import '../App.css';
+import Email from '@material-ui/icons/Email';
+import PhoneIphone from '@material-ui/icons/PhoneIphone';
+
+import {
+  Container,
+  Col,
+  Row
+} from 'react-bootstrap'
+
+function Tag() {
+  return (
+    <div>
+      <p style={{
+        backgroundColor: 'grey',
+        borderRadius: '8px',
+        paddingTop: 6,
+        paddingBottom: 6,
+        paddingRight: 15,
+        paddingLeft: 15,
+        fontSize: 20
+      }}>
+        University of Porto
+      </p>
+    </div>
+  )
+}
 
 function ResumeInfo() {
 
@@ -7,19 +33,62 @@ function ResumeInfo() {
   const [uploadVisible, setUploadVisible] = useState(true)
 
   return (
-    <div className="App">
-            <header className="App-header">
-                
-            </header>
-        </div>
+    <div style={styles.page}>
+      <h1 style={styles.title}>John Smith</h1>
+      <div style={styles.contacts}>
+        <p style={styles.email}>
+          <Email fontSize="large" style={styles.icon} />johnsmith@gmail.com
+        </p>
+        <p>
+          <PhoneIphone fontSize="large" style={styles.icon} />+351 910450865
+        </p>
+      </div>
+      <div style={styles.universities}>
+        <h2 style={styles.subtitle}>Universities: </h2>
+        <Tag />
+      </div>
+      <Container>
+        <Row>
+          <Col sm={4}>Universities</Col>
+          <Col sm={8}><Tag /><Tag /></Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
 const styles = {
+  page: {
+    backgroundColor: '#282c34',
+    display: 'flex',
+    flexDirection: 'column' as 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+  },
+  icon: {
+    paddingRight: 10
+  },
   title: {
+    fontWeight: 'bold',
+    fontSize: 40
+  },
+  subtitle: {
     fontWeight: 'bold',
     fontSize: 30
   },
+  email: {
+    paddingRight: 500
+  },
+  contacts: {
+    display: 'flex',
+    flexDirection: 'row' as 'row',
+    fontSize: 27
+  },
+  universities: {
+    display: 'flex',
+    flexDirection: 'row' as 'row'
+  }
 }
 
 export default ResumeInfo;
